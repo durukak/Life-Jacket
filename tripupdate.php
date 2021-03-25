78e9dd6e40a2c0c268a5706cbe79abff177a3234
durukak <78266708+durukak@users.noreply.github.com>
2021-03-25 20:34:33 +0900

    update css

--- tripupdate.php
+++ tripupdate.php
@@ -1,12 +1,36 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-<title>LIFEJACKET</title>
-<meta charset="utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="UTF-8">
 <link rel="stylesheet"
    href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-   <style type="text/css">
+      <!-- Basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- Mobile Metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <!-- Site Metas -->
+      <title>LIFEJACKET</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- site icon -->
+      <link rel="icon" href="images/fevicon.png" type="image/png" />
+      <!-- Bootstrap core CSS -->
+      <link href="css/bootstrap.css" rel="stylesheet">
+      <!-- FontAwesome Icons core CSS -->
+      <link href="css/font-awesome.min.css" rel="stylesheet">
+      <!-- Custom animate styles for this template -->
+      <link href="css/animate.css" rel="stylesheet">
+      <!-- Custom styles for this template -->
+      <link href="style.css" rel="stylesheet">
+      <!-- Responsive styles for this template -->
+      <link href="css/responsive.css" rel="stylesheet">
+      <!-- Colors for this template -->
+      <link href="css/colors.css" rel="stylesheet">
+      <!-- light box gallery -->
+      <link href="css/ekko-lightbox.css" rel="stylesheet">
+      <style type="text/css">
    input {
        height: 39px;
        font-size: 14px;
@@ -20,11 +44,12 @@
    }
    .mainBoard{
        width: 800px;
-       margin: 50px auto 0px;
+       margin: -100px auto 0px;
    }
    
    body{
       font-size : 20px;
+      color : black;
    }
    .navbar-brand{
       font-size: 30px;
@@ -46,25 +71,70 @@ $res = $con->query($sql);
 $row=mysqli_fetch_array($res);
 ?>
 <?php
-if ((! isset($_SESSION['username'])) && (! isset($_SESSION['name'])) && (! isset($_SESSION['phone']))) {
-?>
+    if ((! isset($_SESSION['username'])) && (! isset($_SESSION['name'])) && (! isset($_SESSION['phone'])))?>
+     <header class="header">
 
-    <div style="padding: 100px;">
-      <div class="navbar-header">
-         <a class="navbar-brand" href="lifejacket.php">LIFE JACKET</a>
-      </div>
-      <nav nav class="nav navbar-nav navbar-right">
-         <ul class="nav navbar-nav">
-            <li><a href="about.html">About Us</a></li>
-            <li><a href="trips.php">Trips</a></li>
-            <li><a href="search.php">Members</a></li>
-            <li><a href="mypage.html">My Page</a></li>
-            <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span>Logout</a></li>
-         </ul>
-      </nav>
+<div class="header_top_section">
+   <div class="container">
+      <div class="row">
+       <div class="col-lg-3">
+          <div class="full">
+             <div class="logo">
+                <a href="lifejacket.php"><img src="images/logo.png" alt="#" /></a>
+             </div>
+          </div>
+       </div>
+       <div class="col-lg-9 site_information">
+          <div class="full">
+             <div class="main_menu">
+                <nav class="navbar navbar-inverse navbar-toggleable-md">
+                   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#cloapediamenu" aria-controls="cloapediamenu" aria-expanded="false" aria-label="Toggle navigation">
+                   <span class="float-left">Menu</span>
+                   <span class="float-right"><i class="fa fa-bars"></i> <i class="fa fa-close"></i></span>
+                   </button>
+                   <div class="collapse navbar-collapse justify-content-md-center" id="cloapediamenu">
+                      <ul class="navbar-nav">
+                         <li class="nav-item active">
+                            <a class="nav-link" href="lifejacket.php">HOME</a>
+                         </li>
+                         <li class="nav-item">
+                            <a class="nav-link color-aqua-hover" href="#">ABOUT</a>
+                         </li>
+                         <li class="nav-item">
+                            <a class="nav-link color-aqua-hover" href="trips.php">TRIP</a>
+                         </li>
+                         <li class="nav-item">
+                            <a class="nav-link color-grey-hover" href="search.php">MEMBER</a>
+                         </li>
+                         <li class="nav-item">
+                            <a class="nav-link color-grey-hover" href="mypage.html">MYPAGE</a>
+                         </li>
+                         <li class="nav-item">
+                            <a class="nav-link color-grey-hover" href="login.html">LOGIN</a>
+                         </li>
+                      </ul>
+                   </div>
+                </nav>
+             </div>
+          </div>
+       </div>
+    </div>
    </div>
-  <?php } ?> 
-   <div class="mainBoard" style="padding-bottom: 50px;">
+</div>
+
+</header>
+            <!-- section -->
+            <section class="main_full banner_section_top">
+        <div class="container-fluid">
+          <div class="row">
+             <div class="full">
+                  <div class="slider_banner">
+                    <img class="img-responsive" src="images/day-planner-828611.jpg" alt="#" />
+                      <div class="slide_cont">
+                        <div class="slider_cont_inner">
+                        <div class="mainBoard" style="padding-bottom: 50px;">
+                        <div class="mainBoard" style="padding-bottom: 50px;">
+                        <div class="mainBoard" style="padding-bottom: 50px;">
       <table class="table table-hover">
          <colgroup>
             <col width="30%">
@@ -110,8 +180,18 @@ if ((! isset($_SESSION['username'])) && (! isset($_SESSION['name'])) && (! isset
          </tr>
       </table>
       <textarea rows="7" cols="75" id="descr"></textarea>
-      <button class="btn btn-default btn-sm btn-block" style="font-size: 20px; font-weight: bolder;" onclick="apply();">Post</button>
+      <button class="btn btn-default btn-sm btn-block" style="font-size: 20px; font-weight: bolder;" onclick="apply();">UPDATE</button>
    </div>
+   </div>
+                        </div>
+                      </div>
+                  </div>
+                </div>
+          </div>
+        </div>
+      </section>
+      <!-- end section -->
+
    <script>
         function apply(){
             var x1 = document.getElementById("locat").value.replace("+","＋").replace(/#/g,"＃").replace(/&/g,"＆").replace(/=/g,"＝").replace(/\\/g,"＼");

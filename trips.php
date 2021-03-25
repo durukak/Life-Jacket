964037e241063a46fdbe3d7f948c09e9b7f338ca
durukak <78266708+durukak@users.noreply.github.com>
2021-03-25 20:33:08 +0900

    update css

--- trips.php
+++ trips.php
@@ -1,12 +1,36 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-<title>LIFEJACKET</title>
-<meta charset="utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="UTF-8">
 <link rel="stylesheet"
    href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-<style type="text/css">
+      <!-- Basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- Mobile Metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <!-- Site Metas -->
+      <title>LIFEJACKET</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- site icon -->
+      <link rel="icon" href="images/fevicon.png" type="image/png" />
+      <!-- Bootstrap core CSS -->
+      <link href="css/bootstrap.css" rel="stylesheet">
+      <!-- FontAwesome Icons core CSS -->
+      <link href="css/font-awesome.min.css" rel="stylesheet">
+      <!-- Custom animate styles for this template -->
+      <link href="css/animate.css" rel="stylesheet">
+      <!-- Custom styles for this template -->
+      <link href="style.css" rel="stylesheet">
+      <!-- Responsive styles for this template -->
+      <link href="css/responsive.css" rel="stylesheet">
+      <!-- Colors for this template -->
+      <link href="css/colors.css" rel="stylesheet">
+      <!-- light box gallery -->
+      <link href="css/ekko-lightbox.css" rel="stylesheet">
+      <style type="text/css">
 input {
    height: 39px;
    font-size: 14px;
@@ -20,21 +44,19 @@ input {
 }
 
 .mainBoard {
-   width: 800px;
-   margin: 50px auto 0px;
+   width: 1000px;
+   margin: -100px auto 0px;
 }
 
 body {
-   font-size: 20px;
+   font-size: 30px;
+   color: #000;
 }
 
 .navbar-brand {
    font-size: 30px;
 }
 </style>
-</head>
-<script
-   src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 <body class="body">
 <?php
@@ -56,31 +78,78 @@ if (isset($_GET['pagination'])) {
 $sql = "select * from trip";
 $res = $con->query($sql);
 $totaltripID = mysqli_num_rows($res); // number of posts
-$totalpagenum = ceil($totaltripID / 10); // numberof pages = number of posts / number of posts per page
+$totalpagenum = ceil($totaltripID / 5); // numberof pages = number of posts / number of posts per page
 $totalblocknum = ceil($totalpagenum / 5); // number of blocks = number of pages / number of pages per block
-$currentpagenum = (($page - 1) * 10); // current page number = (page number -1)*1
-$sql2 = "select tripID, locat from trip order by tripID asc limit $currentpagenum,10"; // display 10 posts in page
+$currentpagenum = (($page - 1) * 5); // current page number = (page number -1)*1
+$sql2 = "select tripID, locat from trip order by tripID asc limit $currentpagenum,5"; // display 10 posts in page
 $res2 = $con->query($sql2);
-$num2 = (($page - 1) * 10) + 1;
+$num2 = (($page - 1) * 5) + 1;
 ?>
 <?php
 if ((! isset($_SESSION['username'])) && (! isset($_SESSION['name'])) && (! isset($_SESSION['phone']))) {
     ?>
-   <div style="padding: 100px;">
-      <div class="navbar-header">
-         <a class="navbar-brand" href="lifejacket.php">LIFE JACKET</a>
-      </div>
-      <nav nav class="nav navbar-nav navbar-right">
-         <ul class="nav navbar-nav">
-            <li><a href="about.html">About Us</a></li>
-            <li><a href="trips.php">Trips</a></li>
-            <Li><a href="search.php">Members</a></li>
-            <li><a href="mypage.html">My Page</a></li>
-            <li><a href="login.html"><span class="glyphicon glyphicon-log-in"></span>Login</a></li>
-         </ul>
-      </nav>
-   </div>
-   <div class="mainBoard" style="padding-bottom: 50px;">
+      <!-- header -->
+      <header class="header">
+
+        <div class="header_top_section">
+           <div class="container">
+              <div class="row">
+               <div class="col-lg-3">
+                  <div class="full">
+                     <div class="logo">
+                        <a href="lifejacket.php"><img src="images/logo.png" alt="#" /></a>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-lg-9 site_information">
+                  <div class="full">
+                     <div class="main_menu">
+                        <nav class="navbar navbar-inverse navbar-toggleable-md">
+                           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#cloapediamenu" aria-controls="cloapediamenu" aria-expanded="false" aria-label="Toggle navigation">
+                           <span class="float-left">Menu</span>
+                           <span class="float-right"><i class="fa fa-bars"></i> <i class="fa fa-close"></i></span>
+                           </button>
+                           <div class="collapse navbar-collapse justify-content-md-center" id="cloapediamenu">
+                              <ul class="navbar-nav">
+                                 <li class="nav-item active">
+                                    <a class="nav-link" href="lifejacket.php">HOME</a>
+                                 </li>
+                                 <li class="nav-item">
+                                    <a class="nav-link color-aqua-hover" href="#">ABOUT</a>
+                                 </li>
+                                 <li class="nav-item">
+                                    <a class="nav-link color-aqua-hover" href="trips.php">TRIP</a>
+                                 </li>
+                                 <li class="nav-item">
+                                    <a class="nav-link color-grey-hover" href="search.php">MEMBER</a>
+                                 </li>
+                                 <li class="nav-item">
+                                    <a class="nav-link color-grey-hover" href="#.html">MYPAGE</a>
+                                 </li>
+                                 <li class="nav-item">
+                                    <a class="nav-link color-grey-hover" href="login.html">LOGIN</a>
+                                 </li>
+                              </ul>
+                           </div>
+                        </nav>
+                     </div>
+                  </div>
+               </div>
+            </div>
+           </div>
+        </div>
+
+      </header>
+            <!-- section -->
+            <section class="main_full banner_section_top">
+        <div class="container-fluid">
+          <div class="row">
+             <div class="full">
+                  <div class="slider_banner">
+                    <img class="img-responsive" src="images/day-planner-828611.jpg" alt="#" />
+                      <div class="slide_cont">
+                        <div class="slider_cont_inner">
+                        <div class="mainBoard" style="padding-bottom: 50px;">
       <table class="table">
          <colgroup>
             <col width="15%">
@@ -92,6 +161,16 @@ if ((! isset($_SESSION['username'])) && (! isset($_SESSION['name'])) && (! isset
                 <th style="text-align: center;">Location</th>
              </tr>
          </thead>
+                        
+                        </div>
+                      </div>
+                  </div>
+                </div>
+          </div>
+        </div>
+      </section>
+      <!-- end section -->
+
   <?php
 
     while ($row = mysqli_fetch_array($res2)) {
@@ -108,7 +187,7 @@ if ((! isset($_SESSION['username'])) && (! isset($_SESSION['name'])) && (! isset
          <tr style="cursor: pointer;"
             onClick="location.href='/tripread.php?x=<?php echo $num;?>'">
             <th style="text-align: center;"><?php echo $num2;?></th>
-            <th><?php if(mb_strlen($row['locat'],"utf-8") > 30) {echo $title;} else {echo $title2;}?></th>
+            <th style="text-align: center;"><?php if(mb_strlen($row['locat'],"utf-8") > 30) {echo $title;} else {echo $title2;}?></th>
          </tr>
   <?php
 
@@ -140,24 +219,69 @@ if ((! isset($_SESSION['username'])) && (! isset($_SESSION['name'])) && (! isset
    </div>
   
 
-  <?php } else {?>
-    <?php
-    if ((! isset($_SESSION['username'])) && (! isset($_SESSION['name'])) && (! isset($_SESSION['phone'])))?>
-     <div style="padding: 100px;">
-      <div class="navbar-header">
-         <a class="navbar-brand" href="lifejacket.php">LIFE JACKET</a>
-      </div>
-      <nav nav class="nav navbar-nav navbar-right">
-         <ul class="nav navbar-nav">
-            <li><a href="about.html">About Us</a></li>
-            <li><a href="trips.php">Trips</a></li>
-            <li><a href="search.php">Members</a></li>
-            <li><a href="mypage.html">My Page</a></li>
-            <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span>Logout</a></li>
-         </ul>
-      </nav>
+   <?php } else {?>
+        <!-- header -->
+      <header class="header">
+
+<div class="header_top_section">
+   <div class="container">
+      <div class="row">
+       <div class="col-lg-3">
+          <div class="full">
+             <div class="logo">
+                <a href="lifejacket.php"><img src="images/logo.png" alt="#" /></a>
+             </div>
+          </div>
+       </div>
+       <div class="col-lg-9 site_information">
+          <div class="full">
+             <div class="main_menu">
+                <nav class="navbar navbar-inverse navbar-toggleable-md">
+                   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#cloapediamenu" aria-controls="cloapediamenu" aria-expanded="false" aria-label="Toggle navigation">
+                   <span class="float-left">Menu</span>
+                   <span class="float-right"><i class="fa fa-bars"></i> <i class="fa fa-close"></i></span>
+                   </button>
+                   <div class="collapse navbar-collapse justify-content-md-center" id="cloapediamenu">
+                      <ul class="navbar-nav">
+                         <li class="nav-item active">
+                            <a class="nav-link" href="lifejacket.php">LIFEJACKET</a>
+                         </li>
+                         <li class="nav-item">
+                            <a class="nav-link color-aqua-hover" href="#">ABOUT</a>
+                         </li>
+                         <li class="nav-item">
+                            <a class="nav-link color-aqua-hover" href="trips.php">TRIP</a>
+                         </li>
+                         <li class="nav-item">
+                            <a class="nav-link color-grey-hover" href="search.php">MEMBER</a>
+                         </li>
+                         <li class="nav-item">
+                            <a class="nav-link color-grey-hover" href="#.html">MYPAGE</a>
+                         </li>
+                         <li class="nav-item">
+                            <a class="nav-link color-grey-hover" href="logout.php">LOGOUT</a>
+                         </li>
+                      </ul>
+                   </div>
+                </nav>
+             </div>
+          </div>
+       </div>
+    </div>
    </div>
-   <div class="mainBoard" style="padding-bottom: 50px;">
+</div>
+
+</header>
+            <!-- section -->
+            <section class="main_full banner_section_top">
+        <div class="container-fluid">
+          <div class="row">
+             <div class="full">
+                  <div class="slider_banner">
+                    <img class="img-responsive" src="images/day-planner-828611.jpg" alt="#" />
+                      <div class="slide_cont">
+                        <div class="slider_cont_inner">
+                        <div class="mainBoard" style="padding-bottom: 50px;">
       <table class="table">
          <colgroup>
             <col width="15%">
@@ -169,6 +293,15 @@ if ((! isset($_SESSION['username'])) && (! isset($_SESSION['name'])) && (! isset
                 <th style="text-align: center;">Location</th>
              </tr>
          </thead>
+                        
+                        </div>
+                      </div>
+                  </div>
+                </div>
+          </div>
+        </div>
+      </section>
+      <!-- end section -->
   <?php
 
     while ($row = mysqli_fetch_array($res2)) {
@@ -179,7 +312,7 @@ if ((! isset($_SESSION['username'])) && (! isset($_SESSION['name'])) && (! isset
   <tr style="cursor: pointer;"
             onClick="location.href='/tripread.php?x=<?php echo $num;?>'">
             <th style="text-align: center;"><?php echo $num2;?></th>
-            <th><?php if(mb_strlen($row['locat'],"utf-8") > 30) {echo $title;} else {echo $title2;}?></th>
+            <th style="text-align: center;"><?php if(mb_strlen($row['locat'],"utf-8") > 30) {echo $title;} else {echo $title2;}?></th>
          </tr>
     <?php $num2++;}?>
   </table>
@@ -205,8 +338,8 @@ if ((! isset($_SESSION['username'])) && (! isset($_SESSION['name'])) && (! isset
     ?>
   <?php } ?> 
   </ul>
-      <button style="float: right;" class="btn-btn-default"
-         onclick="location.href='tripwrite.php'">Add new trip</button>
+      <button style="float: right; border: transparent; background-color: black; color: white" class="btn-btn-default"
+         onclick="location.href='tripwrite.php'">Add trip</button>
    </div>
 </body>
 </html>
